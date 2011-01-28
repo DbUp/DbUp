10d899fb3f986a123edf842037e659a2010283ec
Paul Stovell <paul@paulstovell.com>
2011-01-29 08:19:11 +1000

    Initial projects

---
+++ src/Samples/SampleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleApplication
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
