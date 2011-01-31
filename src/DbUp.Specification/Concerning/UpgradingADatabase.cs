bee9f4510dbcf9697ccc69dba535b105ed036eaa
Jim Burger <jburg79@gmail.com>
2011-02-01 01:32:48 +1030

    Added a test covering a successful upgrade

--- src/DbUp.Specification/Concerning/UpgradingADatabase.cs
+++ src/DbUp.Specification/Concerning/UpgradingADatabase.cs
@@ -1,17 +1,34 @@
 using System;
+using System.Linq;
 using NUnit.Framework;
 using NSubstitute;
 using DbUp.Specification.Contexts;
 
 namespace DbUp.Specification.Concerning
 {
-	[TestFixture()]
+	[TestFixture]
 	public class UpgradingADatabase : GivenAnOutOfDateDatabase
 	{
-		[Test()]
+		[Test]
 		public void ShouldDetermineIfUpgradeIsRequired ()
 		{
 			Assert.IsTrue(DbUpgrader.IsUpgradeRequired(Log));
+		}
+		
+		[Test]
+		public void ShouldLogInformation()
+		{
+			var result = DbUpgrader.PerformUpgrade(Log);
+			var expectedScript = AllScripts.Last();
+			
+			ScriptExecutor.Received().Execute(ConnectionString, expectedScript, Log);
+            VersionTracker.Received().StoreExecutedScript(ConnectionString, expectedScript, Log);
+			
+			Log.ReceivedWithAnyArgs().WriteInformation("");
+			
+			Assert.IsTrue(result.Successful);
+			Assert.AreEqual(expectedScript, result.Scripts.First());
+                    
 		}
 	}
 }
--- src/DbUp.Specification/Contexts/GivenAnOutOfDateDatabase.cs
+++ src/DbUp.Specification/Contexts/GivenAnOutOfDateDatabase.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using DbUp.ScriptProviders;
 using NSubstitute;
+using NUnit.Framework;
 using DbUp.Journal;
 using DbUp.Execution;
 
@@ -14,23 +15,36 @@ namespace DbUp.Specification.Contexts
 		public IJournal VersionTracker;
 		public IScriptProvider ScriptProvider;
 		public ILog Log;
+		public IEnumerable<SqlScript> AllScripts;
 		public const string ConnectionString = "sqlite:memory:";
 		
-		public GivenAnOutOfDateDatabase ()
+		[SetUp]
+		public void SetUp ()
 		{
 			ScriptProvider = Substitute.For<IScriptProvider> ();
 			VersionTracker = Substitute.For<IJournal> ();
 			ScriptExecutor = Substitute.For<IScriptExecutor> ();
 			Log = Substitute.For<ILog> ();
-			
-			ScriptProvider.GetScripts().Returns(new List<SqlScript>() {
+			AllScripts = new List<SqlScript>() {
 				new SqlScript("0001.southwind.sql", "--LITTLE BOBBY DROP TABLES WAS HERE."),
 				new SqlScript("0002.southwind.sql", "CREATE TABLE USERS --AGAIN")
-			});
+			};
+			
+			ScriptProvider.GetScripts().Returns(AllScripts);
 			
 			VersionTracker.GetExecutedScripts(ConnectionString, Log).Returns(new [] {"0001.southwind.sql"});
 			
 			DbUpgrader = new DatabaseUpgrader(ConnectionString, ScriptProvider, VersionTracker, ScriptExecutor);
+		}
+		
+		[TearDown]
+		public void TearDown ()
+		{
+			ScriptExecutor = null;
+			ScriptProvider = null;
+			VersionTracker = null;
+			Log = null;
+			DbUpgrader = null;
 		}
 	}
 }
