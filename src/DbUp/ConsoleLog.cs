bf85c3676ee904f4bb3fb966c4a0bd8ce49967fd
Paul Stovell <paul@paulstovell.com>
2011-01-31 21:45:38 +1000

    XML comments

--- src/DbUp/ConsoleLog.cs
+++ src/DbUp/ConsoleLog.cs
@@ -2,18 +2,36 @@
 
 namespace DbUp
 {
+    /// <summary>
+    /// A log that writes to the console in a colorful way.
+    /// </summary>
     public class ConsoleLog : ILog
     {
+        /// <summary>
+        /// Writes an informational message to the log.
+        /// </summary>
+        /// <param name="format">The format.</param>
+        /// <param name="args">The args.</param>
         public void WriteInformation(string format, params object[] args)
         {
             Write(ConsoleColor.White, format, args);
         }
 
+        /// <summary>
+        /// Writes an error message to the log.
+        /// </summary>
+        /// <param name="format">The format.</param>
+        /// <param name="args">The args.</param>
         public void WriteError(string format, params object[] args)
         {
             Write(ConsoleColor.Red, format, args);
         }
 
+        /// <summary>
+        /// Writes a warning message to the log.
+        /// </summary>
+        /// <param name="format">The format.</param>
+        /// <param name="args">The args.</param>
         public void WriteWarning(string format, params object[] args)
         {
             Write(ConsoleColor.Yellow, format, args);
