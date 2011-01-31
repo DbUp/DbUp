5277c49450f17c540ff5ebf0d4f8e82437bf174b
Paul Stovell <paul@paulstovell.com>
2011-01-31 20:34:39 +1000

    Version update

--- src/Information/VersionInfo.cs
+++ src/Information/VersionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
 
 
