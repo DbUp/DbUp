23179016519a6c14f03cd805d390c387258eb947
Jim Burger <jburg79@gmail.com>
2011-02-21 00:32:24 +1030

    Using less brittle approaches to determining directories, thanks to some excellent advice from @jstangroome

--- build.ps1
+++ build.ps1
@@ -1,6 +1,14 @@
-$buildFile = "build\Build.proj" ;
-$framework = "v4.0.30319" ;
-$fxPath = Join-Path -Path "C:\Windows\Microsoft.NET\Framework\" -ChildPath $framework ;
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+
+$buildFile = "build\Build.proj"
+$scriptRoot = ($MyInvocation.MyCommand.Path | Split-Path | Resolve-Path).ProviderPath
+$absoluteBuildFile = $scriptRoot | Join-Path -ChildPath $buildFile
+
+$fxPath = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").InstallPath
+
 $msbuildPath = Join-Path -Path $fxPath -ChildPath "MSBuild.exe"
 
-& $msbuildpath $buildfile /t:Build /v:m
+& $msbuildpath $absoluteBuildfile /t:Build /v:m
+
+if (-not $?) { throw "MSBuild failed with exit code $LastExitCode" }
