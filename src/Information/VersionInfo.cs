6f6a1ee4aac61845d097930c928903a6ad0f3a09
Jim Burger <jburg79@gmail.com>
2011-02-23 23:21:31 +1030

    Changed 3rd party dependencies (NSubstitute, NUnit) to use nuget + relevant nuget configuration files Added a breaking changes document.

--- src/Information/VersionInfo.cs
+++ src/Information/VersionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.26.0")]
-[assembly: AssemblyFileVersion("1.0.26.0")]
+[assembly: AssemblyVersion("1.0.27.0")]
+[assembly: AssemblyFileVersion("1.0.27.0")]
 
 
