adb256a2e64175281b11c563e68824f23d154bfa
DamianM <damian@damianm.com>
2011-03-10 11:32:56 +1000

    Added FileSystemScriptProvider and tests

---
+++ src/DbUp.Specification/SpecificationFor.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+namespace DbUp.Specification
+{
+    [TestFixture]
+    public abstract class SpecificationFor<T>
+    {
+
+        public T Subject;
+
+        public abstract T Given();
+        public abstract void When();
+
+        [SetUp]
+        public void SetUp()
+        {
+            Subject = Given();
+            When();
+        }
+
+    }
+}
