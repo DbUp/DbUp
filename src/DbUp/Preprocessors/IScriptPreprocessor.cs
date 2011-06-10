109309bd3db4b88541af589fbdd4bf44d1b068e1
Jake Ginnivan <jake@ginnivan.net>
2011-06-10 18:21:42 +0800

    Added preprocessors, and variable substitutions (schema included by default)

---
+++ src/DbUp/Preprocessors/IScriptPreprocessor.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace DbUp.Preprocessors
+{
+    /// <summary>
+    /// Preprocessor that executes before scripts are executed against the database
+    /// </summary>
+    public interface IScriptPreprocessor
+    {
+        /// <summary>
+        /// Performs some proprocessing step on a script
+        /// </summary>
+        string Process(string contents);
+    }
+}
