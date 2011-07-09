2676ecf63e9951a94cf84299cd49e657d4fa8954
Jake Ginnivan <jake@ginnivan.net>
2011-07-09 18:46:46 +0800

    Trying out idea of SqlCePreprocessor to change nvarchar(max) into ntext etc

---
+++ src/DbUp.SqlCe/SqlCePreprocessor.cs
@@ -0,0 +1,15 @@
+﻿using DbUp.Engine;
+
+namespace DbUp.SqlCe
+{
+    /// <summary>
+    /// This preprocessor makes minor adjustments to your sql to make it compatible with SqlCe
+    /// </summary>
+    public class SqlCePreprocessor : IScriptPreprocessor
+    {
+        public string Process(string contents)
+        {
+            return contents.Replace("nvarchar(max)", "ntext");
+        }
+    }
+}
