bde917fdc6c46a07e80c1404e2d8b17d6c0025f2
Paul Stovell <paul@paulstovell.com>
2012-07-22 23:36:50 +0100

    Build.cmd now passes build number

--- build.cmd
+++ build.cmd
@@ -2,6 +2,6 @@
 
 set framework=v4.0.30319
 
-"%SystemDrive%\Windows\Microsoft.NET\Framework\%framework%\MSBuild.exe" build\Build.proj /t:Build /v:m
+"%SystemDrive%\Windows\Microsoft.NET\Framework\%framework%\MSBuild.exe" build\Build.proj /t:Build /v:m /p:build_number=2.0.0
 echo Done
 pause
