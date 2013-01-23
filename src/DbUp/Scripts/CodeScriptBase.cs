34e639df045e54a638d67162f3ab743e20015f7b
Vincent Bouzon <vincent.bouzon@live.fr>
2013-01-23 15:51:28 +0100

    Clean

---
+++ src/DbUp/Scripts/CodeScriptBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DbUp.Builder;
+
+namespace DbUp.Engine
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class CodeScriptBase : IScript
+    {
+        public string Name
+        {
+            get { return this.GetType().FullName + ".cs"; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configuration"></param>
+        public abstract void Execute(UpgradeConfiguration configuration);
+    }
+}
--- src/DbUp/Scripts/SqlScriptGeneratedAtRuntimeBase.cs
+++ src/DbUp/Scripts/SqlScriptGeneratedAtRuntimeBase.cs
@@ -7,25 +7,6 @@ namespace DbUp.Engine
     /// <summary>
     /// 
     /// </summary>
-    public abstract class CodeScriptBase : IScript
-    {
-        public string Name
-        {
-            get { return this.GetType().FullName + ".cs"; }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="configuration"></param>
-        public abstract void Execute(UpgradeConfiguration configuration);
-    }
-
-
-
-    /// <summary>
-    /// 
-    /// </summary>
     public abstract class SqlScriptGeneratedAtRuntimeBase : IScript
     {
         public string Name
