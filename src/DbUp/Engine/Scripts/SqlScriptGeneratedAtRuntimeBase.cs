31f50a285a05750b44bbf00cf9f3b04ed3ceb5ee
Vincent Bouzon <vincent.bouzon@live.fr>
2013-05-23 12:10:52 +0200

    SqlScriptGeneratedAtRuntime inherit from CodeScriptBase

--- src/DbUp/Engine/Scripts/SqlScriptGeneratedAtRuntimeBase.cs
+++ src/DbUp/Engine/Scripts/SqlScriptGeneratedAtRuntimeBase.cs
@@ -7,14 +7,14 @@ namespace DbUp.Engine
     /// <summary>
     /// 
     /// </summary>
-    public abstract class SqlScriptGeneratedAtRuntimeBase : IScript
+    public abstract class SqlScriptGeneratedAtRuntimeBase : CodeScriptBase
     {
         public string Name
         {
             get { return this.GetType().FullName + ".cs"; }
         }
 
-        public void Execute(UpgradeConfiguration configuration)
+        public override void Execute(UpgradeConfiguration configuration)
         {
             new SqlScript(Name, ProvideScript(configuration.ConnectionFactory())).Execute(configuration);
         }
