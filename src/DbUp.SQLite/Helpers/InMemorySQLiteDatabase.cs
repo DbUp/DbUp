976dbcb32170ffcf62719d39069521fc031281bd
Hojjat Khodabakhsh <hodjatkh@gmail.com>
2013-05-31 02:11:15 +1000

    # Two helper classes (TemporarySQLiteDatabase, InMemorySQLiteDatabase) added

---
+++ src/DbUp.SQLite/Helpers/InMemorySQLiteDatabase.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using DbUp.Helpers;
+
+namespace DbUp.Sqlite.Helpers
+{
+    /// <summary>
+    /// Used to create in-memory SQLite database that is deleted at the end of a test.
+    /// </summary>
+    public class InMemorySQLiteDatabase : IDisposable
+    {
+        private readonly SQLiteConnectionAdapter sharedConnection;
+        private readonly AdHocSqlRunner sqlRunner;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InMemorySQLiteDatabase"/> class.
+        /// </summary>
+        public InMemorySQLiteDatabase()
+        {
+            var connectionStringBuilder = new SQLiteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = ":memory:";
+            connectionStringBuilder.Version = 3;
+            connectionStringBuilder.DefaultTimeout = 5;
+            connectionStringBuilder.JournalMode = SQLiteJournalModeEnum.Memory;
+            connectionStringBuilder.UseUTF16Encoding = true;
+
+            sharedConnection = new SQLiteConnectionAdapter(new SQLiteConnection(connectionStringBuilder.ConnectionString));
+            sqlRunner = new AdHocSqlRunner(GetConnectionFactory(), null, () => true);
+        }
+
+        /// <summary>
+        /// Gets the connection factory of in-memory database.
+        /// </summary>
+        public Func<IDbConnection> GetConnectionFactory()
+        {
+            return () => (IDbConnection)sharedConnection;
+        }
+
+        /// <summary>
+        /// An ahoc sql runner against the in-memory database
+        /// </summary>
+        public AdHocSqlRunner SqlRunner
+        {
+            get { return sqlRunner; }
+        }
+
+        /// <summary>
+        /// remove the database from memory
+        /// </summary>
+        public void Dispose()
+        {
+            sharedConnection.DisposeConnection();
+        }
+
+        /// <summary>
+        /// SQLite in-memory database ceases to exist when database connection is closed, 
+        /// hence SQLite connection is wrapped using this class to prevent it to be closed 
+        /// until the database is explicitly disposed.
+        /// </summary>
+        private class SQLiteConnectionAdapter : IDbConnection
+        {
+            private readonly SQLiteConnection sqliteConnection;
+            
+            public SQLiteConnectionAdapter(SQLiteConnection sqliteConnection)
+            {
+                this.sqliteConnection = sqliteConnection;
+            }
+
+            public IDbTransaction BeginTransaction(IsolationLevel il)
+            {
+                return sqliteConnection.BeginTransaction(il);
+            }
+
+            public IDbTransaction BeginTransaction()
+            {
+                return sqliteConnection.BeginTransaction();
+            }
+
+            public void ChangeDatabase(string databaseName)
+            {
+                sqliteConnection.ChangeDatabase(databaseName);
+            }
+
+            public void Open()
+            {
+                if (sqliteConnection.State == ConnectionState.Closed)
+                    sqliteConnection.Open();
+            }
+
+            public void Close()
+            {
+                // don't close connection becasue it ceases the cache database existance
+            }
+
+            public string ConnectionString
+            {
+                get
+                {
+                    return sqliteConnection.ConnectionString;
+                }
+                set
+                {
+                    sqliteConnection.ConnectionString = value;
+                }
+            }
+
+            public int ConnectionTimeout
+            {
+                get { return sqliteConnection.ConnectionTimeout; }
+            }
+
+            public IDbCommand CreateCommand()
+            {
+                return sqliteConnection.CreateCommand();
+            }
+
+            public string Database
+            {
+                get { return sqliteConnection.Database; }
+            }
+
+            public ConnectionState State
+            {
+                get { return sqliteConnection.State; }
+            }
+
+            public void Dispose()
+            {
+                // don't close or dispose the connection becasue it ceases the cache database existance
+            }
+
+            public void DisposeConnection()
+            {
+                // Close and dispose sqlite connection when this method is called
+                try
+                {
+                    sqliteConnection.Close();
+                }
+                finally
+                {
+                    sqliteConnection.Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ src/DbUp.SQLite/Helpers/TemporarySQLiteDatabase.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using DbUp.Helpers;
+using System.Data.SQLite;
+
+namespace DbUp.Sqlite.Helpers
+{
+    /// <summary>
+    /// Used to create SQLite databases that are deleted at the end of a test.
+    /// </summary>
+    public class TemporarySQLiteDatabase : IDisposable
+    {
+        private readonly string dataSourcePath;
+        private readonly string connectionString;
+        private readonly AdHocSqlRunner sqlRunner;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TemporarySQLiteDatabase"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        public TemporarySQLiteDatabase(string name)
+        {
+            dataSourcePath = Path.Combine(Environment.CurrentDirectory, name);
+
+            var connectionStringBuilder = new SQLiteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = name;
+            connectionStringBuilder.Version = 3;
+            connectionStringBuilder.DefaultTimeout = 5;
+            connectionStringBuilder.JournalMode = SQLiteJournalModeEnum.Memory;
+            connectionStringBuilder.UseUTF16Encoding = true;
+
+            connectionString = connectionStringBuilder.ConnectionString;
+            sqlRunner = new AdHocSqlRunner(() => new SQLiteConnection(connectionString), null, () => true);
+        }
+
+        /// <summary>
+        /// Gets the connection string of temporary database.
+        /// </summary>
+        public string ConnectionString
+        {
+            get { return connectionString; }
+        }
+
+        /// <summary>
+        /// An ahoc sql runner against the temporary database
+        /// </summary>
+        public AdHocSqlRunner SqlRunner
+        {
+            get { return sqlRunner; }
+        }
+
+        /// <summary>
+        /// Creates the database.
+        /// </summary>
+        public void Create()
+        {
+            var filePath = new FileInfo(dataSourcePath);
+            if (!filePath.Exists)
+            {
+                SQLiteConnection.CreateFile(dataSourcePath);
+            }
+        }
+
+        /// <summary>
+        /// Deletes the database.
+        /// </summary>
+        public void Dispose()
+        {
+            var filePath = new FileInfo(dataSourcePath);
+            if (filePath.Exists)
+            {
+                SQLiteConnection.ClearAllPools();
+
+                // SQLite requires all created sql connection/command objects to be disposed
+                // in order to delete the database file
+                GC.Collect(2, GCCollectionMode.Forced);
+                System.Threading.Thread.Sleep(100);
+
+                File.Delete(dataSourcePath);
+            }
+        }
+    }
+}
