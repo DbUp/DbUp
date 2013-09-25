895ec0500573a40e9c7d1bfeb4694421388edb68
Steve Godbold <stephen.godbold@gmail.com>
2013-09-25 21:09:14 +1000

    Added null journal for omnipotent script execution

---
+++ src/DbUp.Tests/NullJournalTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using DbUp.Helpers;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DbUp.Tests
+{
+    [TestFixture]
+    public class NullJournalTests
+    {
+        [Test]
+        public void shouldnt_journal_anything_executed()
+        {
+            var journal = new NullJournal();
+            var connection = Substitute.For<IDbConnection>();
+            var command = Substitute.For<IDbCommand>();
+            connection.CreateCommand().Returns(command);
+
+            var upgradeEngine = DeployChanges.To
+                .SqlDatabase(() => connection, "Db")
+                .WithScript("testscript", "SELECT * FROM BLAH")
+                .JournalTo(journal)
+                .Build();
+
+            upgradeEngine.PerformUpgrade();
+
+            Assert.AreEqual(0, journal.GetExecutedScripts().Length);
+        }
+    }
+}
---
+++ src/DbUp/Helpers/NullJournal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DbUp.Engine;
+
+namespace DbUp.Helpers
+{
+    /// <summary>
+    /// Enables multiple executions of a set of scripts.
+    /// </summary>
+    public class NullJournal : IJournal
+    {
+        /// <summary>
+        /// Returns an empy array of length 0 to ensure all 
+        /// scripts are run
+        /// </summary>
+        /// <returns></returns>
+        public string[] GetExecutedScripts()
+        {
+            return new string[0];
+        }
+
+        /// <summary>
+        /// Does not store the script, simply returns
+        /// </summary>
+        /// <param name="script"></param>
+        public void StoreExecutedScript(SqlScript script)
+        { }
+    }
+}
