d3360b3d1e442caa90e47ff46bca851355616b60
DamianM <damian@damianm.com>
2014-03-15 00:22:25 +1000

    comment parsing

--- src/DbUp.Tests/Engine/ScriptParsingTests.cs
+++ src/DbUp.Tests/Engine/ScriptParsingTests.cs
@@ -8,6 +8,7 @@ namespace DbUp.Tests.Engine
 {
     public class ScriptParsingTests
     {
+
         [TestFixture]
         public class When_Parsing_A_Script_With_GO_separators : SpecificationFor<SqlBatchParser>
         {
@@ -20,7 +21,7 @@ namespace DbUp.Tests.Engine
 
             public override void When()
             {
-                var sql = @"SELECT 1
+                string sql = @"SELECT 1
 GO
 SELECT 2";
                 batches = Subject.SplitScriptBatches(sql, "GO");
@@ -35,11 +36,43 @@ SELECT 2";
             [Then]
             public void it_returns_the_right_content()
             {
-                Assert.AreEqual(new String[]{"SELECT 1", "SELECT 2"}, batches);
+                Assert.AreEqual(new[] {"SELECT 1", "SELECT 2"}, batches);
+            }
+        }
+
+
+        [TestFixture]
+        public class When_parsing_a_script_with_comments : SpecificationFor<SqlBatchParser>
+        {
+            private IEnumerable<string> batches;
+
+            public override SqlBatchParser Given()
+            {
+                return new SqlBatchParser();
             }
 
+            public override void When()
+            {
+                string sql = @"SELECT 1
+/*
 
+FOO
+GO
+SELECT 2
+*/
+GO
+SELECT 3
+GO
+SELECT 4";
+                batches = Subject.SplitScriptBatches(sql, "GO");
+            }
+
+            [Then]
+            public void it_strips_comments_and_parses_correctly()
+            {
+                Assert.AreEqual(new[] {"SELECT 1", "SELECT 3", "SELECT 4"}, batches);
+            }
         }
- 
+
     }
 }
--- src/DbUp/Engine/SqlBatchParser.cs
+++ src/DbUp/Engine/SqlBatchParser.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -6,26 +6,33 @@ using System.Text.RegularExpressions;
 namespace DbUp.Engine
 {
     /// <summary>
-    /// Splits scripts based on their batch separator and comments
+    ///     Splits scripts based on their batch separator and comments
     /// </summary>
     public class SqlBatchParser
     {
         /// <summary>
-        /// 
+        ///     Parses SQL Scripts based on batch separators (i.e. GO or ; and removes comments)
         /// </summary>
-        /// <param name="scriptContents"></param>
-        /// <param name="batchSeparator"></param>
+        /// <param name="scriptContents">SQL Script Body</param>
+        /// <param name="batchSeparator">GO or ;</param>
         /// <returns></returns>
         public IEnumerable<string> SplitScriptBatches(string scriptContents, string batchSeparator)
         {
-            var pattern = "^\\s*" + batchSeparator + "\\s*$";
+            scriptContents = StripComments(scriptContents);
+
+            string pattern = "^\\s*" + batchSeparator + "\\s*$";
 
             return Regex.Split(scriptContents, pattern, RegexOptions.IgnoreCase | RegexOptions.Multiline)
                 .Select(x => x.Trim())
                 .Where(x => x.Length > 0)
                 .ToArray();
-
         }
-         
+
+        private string StripComments(string scriptContents)
+        {
+            const string commentPattern = @"/\*(.*?)\*/";
+
+            return Regex.Replace(scriptContents, commentPattern, "", RegexOptions.Singleline);
+        }
     }
 }
