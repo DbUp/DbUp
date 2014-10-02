e7affdaefb9210eec61a7c29dba0d9306eb68f4d
Liam McLennan <liam.mclennan@gmail.com>
2014-10-02 21:24:04 +1000

    start of Postgresql version

---
+++ src/DbUp.Postgresql/PostgresqlConnectionManager.cs
@@ -0,0 +1,38 @@
+﻿using DbUp.Engine.Output;
+using DbUp.Engine.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DbUp.Postgresql
+{
+    public class PostgresqlConnectionManager : DatabaseConnectionManager
+    {
+        private readonly string connectionString;
+
+        public PostgresqlConnectionManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        protected override IDbConnection CreateConnection(IUpgradeLog log)
+        {
+            throw new NotImplementedException();
+            //return new PostgresqlConnection(connectionString);
+        }
+
+        public override IEnumerable<string> SplitScriptIntoCommands(string scriptContents)
+        {
+            var scriptStatements =
+                Regex.Split(scriptContents, "^\\s*;\\s*$", RegexOptions.IgnoreCase | RegexOptions.Multiline)
+                    .Select(x => x.Trim())
+                    .Where(x => x.Length > 0)
+                    .ToArray();
+
+            return scriptStatements;
+        }
+    }
+}
--- src/DbUp.Postgresql/PostgresqlExtensions.cs
+++ src/DbUp.Postgresql/PostgresqlExtensions.cs
@@ -4,11 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DbUp.Postgresql;
+using DbUp.Engine.Transactions;
 
 public static class PostgresqlExtensions
 {
-    //public static UpgradeEngineBuilder PostgresqlDatabase(this SupportedDatabases supported, string connectionString)
-    //{
+    public static UpgradeEngineBuilder PostgresqlDatabase(this SupportedDatabases supported, string connectionString)
+    {
+        return PostgresqlDatabase(new PostgresqlConnectionManager(connectionString));
+    }
 
-    //}
+    public static UpgradeEngineBuilder PostgresqlDatabase(IConnectionManager connectionManager)
+    {
+        throw new NotImplementedException();
+        //var builder = new UpgradeEngineBuilder();
+        //builder.Configure(c => c.ConnectionManager = connectionManager);
+        //builder.Configure(c => c.ScriptExecutor = new SqlScriptExecutor(() => c.ConnectionManager, () => c.Log, null, () => c.VariablesEnabled, c.ScriptPreprocessors));
+        //builder.Configure(c => c.Journal = new SqlTableJournal(() => connectionManager, () => c.Log, null, "SchemaVersions"));
+        //builder.WithPreprocessor(new SqlCePreprocessor());
+        //return builder;
+    }
 }
