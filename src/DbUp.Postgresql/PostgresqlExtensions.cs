4aefa1081f50e05591768aee4abad658f95224a3
Liam McLennan <liam.mclennan@gmail.com>
2014-10-02 21:02:18 +1000

    added postgresql project

---
+++ src/DbUp.Postgresql/PostgresqlExtensions.cs
@@ -0,0 +1,14 @@
+﻿using DbUp.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class PostgresqlExtensions
+{
+    //public static UpgradeEngineBuilder PostgresqlDatabase(this SupportedDatabases supported, string connectionString)
+    //{
+
+    //}
+}
