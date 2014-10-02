76f596732737d460b02f935dd2f58b9dd8ff47da
Liam McLennan <liam.mclennan@gmail.com>
2014-10-02 20:53:56 +1000

    added not-yet-implemented test for postgresql support

---
+++ src/DbUp.Tests/Support/Postgresql/PostgresqlSupportTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DbUp.Tests.Support.Postgresql
+{
+    [TestFixture]
+    public class PostgresqlSupportTests
+    {
+        [Test]
+        public void CanUpgradeAPostgresqlDatabase()
+        {
+            //var upgrader = DeployChanges.To
+            //    .PostgresqlDatabase("")
+            //    .WithScript("Script0001", "create table $schema$.Foo (Id int)")
+            //    .Build();
+
+            //var result = upgrader.PerformUpgrade();
+
+            //Assert.IsTrue(result.Successful);
+        }
+    }
+}
