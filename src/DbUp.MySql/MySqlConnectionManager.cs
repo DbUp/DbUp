c2845c91fad1791b78bb0ca6e11a8c52b23d725a
Chris Bernholt <cbernhlt@hotmail.com>
2014-12-09 18:37:26 -0500

    Add tests, refactor to handle multi-line scripts, refactor to  handle script with single DELIMITER statement

--- src/DbUp.MySql/MySqlConnectionManager.cs
+++ src/DbUp.MySql/MySqlConnectionManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 using DbUp.Engine.Transactions;
@@ -11,6 +12,7 @@ namespace DbUp.MySql
     /// </summary>
     public class MySqlConnectionManager : DatabaseConnectionManager
     {
+        private readonly Regex regex = new Regex(@"DELIMITER (.+)\r\n", RegexOptions.IgnoreCase | RegexOptions.Multiline);
         /// <summary>
         /// Creates a new MySql database connection.
         /// </summary>
@@ -20,18 +22,52 @@ namespace DbUp.MySql
         }
 
         /// <summary>
-        /// Splits the statements in the script using the ";" character.
+        /// Splits the statements in the script using the ";" character or 
+        /// DELIMITER if specified.
         /// </summary>
         /// <param name="scriptContents">The contents of the script to split.</param>
         public override IEnumerable<string> SplitScriptIntoCommands(string scriptContents)
         {
-            var scriptStatements =
-                Regex.Split(scriptContents, "^\\s*;\\s*$", RegexOptions.IgnoreCase | RegexOptions.Multiline)
+            var delimiterMatch = regex.Match(scriptContents);
+
+            if (!delimiterMatch.Success)
+            {
+                return scriptContents.Split(';')
+                    .Select(x => x.Trim())
+                    .Where(x => x.Length > 0).ToList();
+            }
+            else
+            {
+                return GetDelimitedCommands(scriptContents);
+            }
+        }
+
+        private IEnumerable<string> GetDelimitedCommands(string scriptContents)
+        {
+            var delimiterMatch = regex.Match(scriptContents);
+
+            var nonDelimiterScriptContents = regex.Split(scriptContents)
                     .Select(x => x.Trim())
                     .Where(x => x.Length > 0)
                     .ToArray();
 
+            var standardDelimiterString = nonDelimiterScriptContents[0];
+
+            var delimiter = delimiterMatch.Groups[1].Value;
+
+            var newDelimiterString = nonDelimiterScriptContents[2];
+
+            var scriptStatements =
+                standardDelimiterString.Split(';')
+                    .Select(x => x.Trim())
+                    .Where(x => x.Length > 0).ToList();
+
+                scriptStatements.AddRange(newDelimiterString.Split(delimiter.ToArray(), StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .Where(x => x.Length > 0));
+
             return scriptStatements;
+
         }
     }
 }
