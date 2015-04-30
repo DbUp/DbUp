f4f998e0b553c2a0e04e8efbbd787474f90efa06
dazinator <darrell.tunnell@googlemail.com>
2015-04-30 14:11:35 +0100

    A SQLLite provider for Mono. closes #92

---
+++ src/DbUp.Tests/Support/SQLiteMono/SQLiteSupportTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+using Mono.Data.Sqlite;
+using NUnit.Framework;
+
+namespace DbUp.Tests.Support.SQLiteMono
+{
+    [TestFixture]
+    public class SQLiteMonoSupportTests
+    {
+        private static readonly string dbFilePath = Path.Combine(Environment.CurrentDirectory, "test.db");
+
+        [Test]
+        public void CanUseSQLite()
+        {
+            string connectionString = string.Format("Data Source={0}; Version=3;", dbFilePath);
+
+            if (!File.Exists(dbFilePath))
+            {
+                SqliteConnection.CreateFile(dbFilePath);
+            }
+
+            var upgrader = DeployChanges.To
+                .SQLiteMonoDatabase(connectionString)
+                .WithScript("Script0001", "CREATE TABLE IF NOT EXISTS Foo (Id int)")
+                .Build();
+        }
+    }
+}
