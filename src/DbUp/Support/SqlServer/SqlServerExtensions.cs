ac2fd73a8aeedb53337db76eb47d04cde19e5d75
Rick Culpepper <rculpepper@campingworld.com>
2015-09-07 08:12:45 -0500

    Adding EnsureDatabase.For fluent API. Implemented for SQL Server, all others throw NotImplementedException(). Added to Console.

--- src/DbUp/Support/SqlServer/SqlServerExtensions.cs
+++ src/DbUp/Support/SqlServer/SqlServerExtensions.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Text;
+using DbUp;
 using DbUp.Builder;
 using DbUp.Engine.Transactions;
 using DbUp.Support.SqlServer;
@@ -97,4 +101,77 @@ public static class SqlServerExtensions
         builder.Configure(c => c.Journal = new SqlTableJournal(()=>c.ConnectionManager, ()=>c.Log, schema, table));
         return builder;
     }
+
+    /// <summary>
+    /// Ensures that the database specified in the connection string exists.
+    /// </summary>
+    /// <param name="supported">Fluent helper type.</param>
+    /// <param name="connectionString">The connection string.</param>
+    /// <returns></returns>
+    public static void SqlDatabase(this SupportedDatabasesForEnsureDatabase supported, string connectionString)
+    {
+        if (string.IsNullOrEmpty(connectionString) || connectionString.Trim() == string.Empty)
+        {
+            throw new ArgumentNullException("connectionString");
+        }
+
+        var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+
+        var databaseName = connectionStringBuilder.InitialCatalog;
+
+        if (string.IsNullOrEmpty(databaseName) || databaseName.Trim() == string.Empty)
+        {
+            throw new InvalidOperationException("The connection string does not specify a database name.");
+        }
+
+        connectionStringBuilder.InitialCatalog = "master";
+
+        // TODO: Where to log output from this method? There is no Log context at this point.
+        Debug.WriteLine("Master ConnectionString => {0}", connectionStringBuilder.ToString());
+
+        using (var connection = new SqlConnection(connectionStringBuilder.ConnectionString))
+        {
+            connection.Open();
+            
+            var sqlCommandText = string.Format
+                (
+                    @"select case when db_id('{0}') is not null then 1 else 0 end;",
+                    databaseName
+                );
+
+
+            // check to see if the database already exists..
+            using (var command = new SqlCommand(sqlCommandText, connection)
+            {
+                CommandType = CommandType.Text
+            })
+            {
+                var results = (int) command.ExecuteScalar();
+
+                // if the database exists, we're done here...
+                if (results == 1) return;
+            }
+
+            sqlCommandText = string.Format
+                    (
+                        @"create database [{0}];",
+                        databaseName
+                    );
+
+            // Create the database...
+            using (var command = new SqlCommand(sqlCommandText, connection)
+            {
+                CommandType = CommandType.Text
+            })
+            {
+                command.ExecuteNonQuery();
+
+            }
+
+            // TODO: Where to log output from this method? There is no Log context at this point.
+            Console.WriteLine(@"Created database {0}", databaseName);
+        }
+    }
+
+
 }
