ec3bcf9b6ffa226a5fc755097677b4f3b8c80476
Jake Ginnivan <jake@ginnivan.net>
2015-09-11 09:32:48 +0800

    Switched projects to used shared solution info and shared versioninfo

--- src/DbUp.Firebird/Properties/AssemblyInfo.cs
+++ src/DbUp.Firebird/Properties/AssemblyInfo.cs
@@ -3,12 +3,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DbUp.Firebird")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DbUp Contributors")]
-[assembly: AssemblyProduct("DbUp.Firebird")]
-[assembly: AssemblyCopyright("Copyright © DbUp Contributors 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
 [assembly: Guid("a0df89fb-5f3e-4690-af40-acf2816e89f1")]
-[assembly: AssemblyVersion("1.0.0.0")]
--- src/DbUp.Tests/DbUp.Postgresql.approved.cs
+++ src/DbUp.Tests/DbUp.Postgresql.approved.cs
@@ -1,4 +1,5 @@
-﻿[assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
+﻿[assembly: System.CLSCompliantAttribute(true)]
+[assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
 [assembly: System.Runtime.InteropServices.GuidAttribute("5ddc04cc-0bd3-421e-9ae4-9fd0e4f4ef04")]
 [assembly: System.Runtime.Versioning.TargetFrameworkAttribute(".NETFramework,Version=v4.0", FrameworkDisplayName=".NET Framework 4")]
 
