04ab4d1397a3b75fc49e5c62322d755e3015767d
Philippe Tremblay <Philippe.Tremblay@ElapseTech.com>
2016-05-12 15:34:17 -0400

    New option to include scripts from subdirectories under the path used by the FileSystemProvider

---
+++ src/DbUp.Tests/ScriptProvider/TestScripts.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using ApprovalUtilities.Utilities;
+
+namespace DbUp.Tests.ScriptProvider
+{
+    static class TestScripts
+    {
+        public static void Create(out string testPath)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            testPath = CreateTestPathBasedOnAssemblyLocation(assembly);
+
+            foreach (var scriptName in assembly.GetManifestResourceNames().Where(f => f.Contains(".sql")))
+            {
+                using (var stream = assembly.GetManifestResourceStream(scriptName))
+                {
+                    var filePath = Path.Combine(testPath, GetScriptPathAndName(scriptName));
+                    Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                    using (var writer = new FileStream(filePath, FileMode.Create))
+                    {
+                        stream.CopyTo(writer);
+                        writer.Flush();
+                        writer.Close();
+                    }
+                    stream.Close();
+                }
+            }
+        }
+
+        private static string GetScriptPathAndName(string scriptName)
+        {
+            var dir = Regex.Match(scriptName, @"\.(Folder\d)");
+            if (dir.Success)
+            {
+                return Path.Combine(dir.Groups[1].Value, scriptName.Replace(dir.Value, ""));
+            }
+            return scriptName;
+        }
+
+        private static string CreateTestPathBasedOnAssemblyLocation(Assembly assembly)
+        {
+            var directory = new FileInfo(assembly.Location).DirectoryName;
+            var testPath = Path.Combine(directory, "sqlfiles");
+            Directory.CreateDirectory(testPath);
+            return testPath;
+        }
+    }
+}
---
+++ src/DbUp/ScriptProviders/FileSystemScriptOptions.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Text;
+
+namespace DbUp.ScriptProviders
+{
+    /// <summary>
+    /// Options for the <see cref="FileSystemScriptProvider"/>
+    /// </summary>
+    public class FileSystemScriptOptions
+    {
+        public FileSystemScriptOptions()
+        {
+            Encoding = Encoding.Default;
+        }
+        /// <summary>
+        /// The provider will look in subdirectories for scripts files.
+        /// </summary>
+        public bool IncludeSubDirectories { get; set; }
+
+        /// <summary>
+        /// The filter to be used for filtering files 
+        /// <remarks> Files which does not end by .sql are never considered </remarks>
+        /// </summary>
+        public Func<string, bool> Filter { get; set; }
+
+        /// <summary>
+        /// The encoding to be used for reading files 
+        /// </summary>
+        public Encoding Encoding { get; set; }
+    }
+}
