abeda51c1215bb12fbde92340a4f6ed764d2d7ce
Juliano Bortolozzo Solanho <juliano.solanho@gmail.com>
2016-06-14 16:22:18 -0300

    Updating public API after VariableSubstitutionPreprocessor rework.

--- src/DbUp.Tests/DbUp.approved.cs
+++ src/DbUp.Tests/DbUp.approved.cs
@@ -232,10 +232,6 @@ namespace DbUp.Helpers
         public string[] GetExecutedScripts() { }
         public void StoreExecutedScript(DbUp.Engine.SqlScript script) { }
     }
-    public class static SqlCommentRangeFinder
-    {
-        public static System.Collections.Generic.List<System.Collections.Generic.KeyValuePair<int, int>> FindRanges(string str) { }
-    }
     public class TemporarySqlDatabase : System.IDisposable
     {
         public TemporarySqlDatabase(string name) { }
@@ -337,32 +333,11 @@ namespace DbUp.Support.SQLite
 namespace DbUp.Support.SqlServer
 {
     
-    public class SqlCommandReader : System.IO.StringReader
+    public class SqlCommandReader : DbUp.Support.SqlServer.SqlParser
     {
         protected const int FailedRead = -1;
         public SqlCommandReader(string sqlText, string delimiter = "GO", bool delimiterRequiresWhitespace = True) { }
-        protected char CurrentChar { get; }
-        protected int CurrentIndex { get; }
-        protected string Delimiter { get; set; }
-        protected bool DelimiterRequiresWhitespace { get; set; }
-        protected bool HasReachedEnd { get; }
-        protected virtual bool IsCustomStatement { get; }
-        protected bool IsEndOfLine { get; }
-        protected bool IsQuote { get; }
-        protected bool IsWhiteSpace { get; }
-        protected char LastChar { get; }
-        protected bool IsCharEqualTo(char comparisonChar, char compareTo) { }
-        protected bool IsCurrentCharEqualTo(char comparisonChar) { }
-        protected bool IsLastCharEqualTo(char comparisonChar) { }
-        protected char PeekChar() { }
-        public override int Read() { }
-        public override int Read(char[] buffer, int index, int count) { }
         public void ReadAllCommands(System.Action<string> handleCommand) { }
-        public override int ReadBlock(char[] buffer, int index, int count) { }
-        protected virtual void ReadCustomStatement() { }
-        public override string ReadLine() { }
-        public override string ReadToEnd() { }
-        protected bool TryPeek(int numberOfCharacters, out string result) { }
     }
     public class SqlCommandSplitter
     {
@@ -379,6 +354,47 @@ namespace DbUp.Support.SqlServer
         public SqlObjectParser() { }
         public static string QuoteSqlObjectName(string objectName) { }
         public static string QuoteSqlObjectName(string objectName, DbUp.Support.ObjectNameOptions objectNameOptions) { }
+    }
+    public abstract class SqlParser : System.IO.StringReader
+    {
+        protected const int FailedRead = -1;
+        public SqlParser(string sqlText, string delimiter = "GO", bool delimiterRequiresWhitespace = True) { }
+        protected char CurrentChar { get; }
+        protected int CurrentIndex { get; }
+        protected string Delimiter { get; set; }
+        protected bool DelimiterRequiresWhitespace { get; set; }
+        protected bool HasReachedEnd { get; }
+        protected virtual bool IsCustomStatement { get; }
+        protected bool IsEndOfLine { get; }
+        protected bool IsQuote { get; }
+        protected bool IsWhiteSpace { get; }
+        protected char LastChar { get; }
+        public event System.Action CommandEnded;
+        public event System.Action CommandStarted;
+        public event System.Action<DbUp.Support.SqlServer.SqlParser.CharacterType, char> ReadCharacter;
+        protected bool IsCharEqualTo(char comparisonChar, char compareTo) { }
+        protected bool IsCurrentCharEqualTo(char comparisonChar) { }
+        protected bool IsLastCharEqualTo(char comparisonChar) { }
+        protected void OnReadCharacter(DbUp.Support.SqlServer.SqlParser.CharacterType type, char c) { }
+        protected void Parse() { }
+        protected char PeekChar() { }
+        public override int Read() { }
+        public override int Read(char[] buffer, int index, int count) { }
+        public override int ReadBlock(char[] buffer, int index, int count) { }
+        protected virtual void ReadCustomStatement() { }
+        public override string ReadLine() { }
+        public override string ReadToEnd() { }
+        protected bool TryPeek(int numberOfCharacters, out string result) { }
+        public enum CharacterType
+        {
+            Command = 0,
+            SlashStarComment = 1,
+            DashComment = 2,
+            BracketedText = 3,
+            QuotedString = 4,
+            Delimiter = 5,
+            CustomStatement = 6,
+        }
     }
     public class SqlScriptExecutor : DbUp.Engine.IScriptExecutor
     {
