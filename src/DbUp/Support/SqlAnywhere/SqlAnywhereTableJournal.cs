f069239650eaaf827cbdd7051ce5d7e98365a803
Philippe Tremblay <jesuissur@gmail.com>
2016-09-08 15:27:51 -0400

    Fix VerifyTableExistsCommand for the SqlAnywhereJournal

--- src/DbUp/Support/SqlAnywhere/SqlAnywhereTableJournal.cs
+++ src/DbUp/Support/SqlAnywhere/SqlAnywhereTableJournal.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
 using DbUp.Engine;
 using DbUp.Engine.Output;
 using DbUp.Engine.Preprocessors;
@@ -14,7 +11,7 @@ namespace DbUp.Support.SqlAnywhere
     public class SqlAnywhereTableJournal : SqlTableJournal
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="SqlAnywhereTableJournal"/> class.
+        ///     Initializes a new instance of the <see cref="SqlAnywhereTableJournal" /> class.
         /// </summary>
         /// <param name="connectionManager">The connection manager.</param>
         /// <param name="logger">The log.</param>
@@ -22,7 +19,8 @@ namespace DbUp.Support.SqlAnywhere
         /// <param name="table">The table name.</param>
         /// <param name="sqlProcessor">the sql preprocessor to transform sql before execution</param>
         /// <example>
-        /// var journal = new TableJournal("DBN=MyDatabaseName;UID=DBA;PWD=sql", "schemaNameForVersionTable", "MyVersionTable");
+        ///     var journal = new TableJournal("DBN=MyDatabaseName;UID=DBA;PWD=sql", "schemaNameForVersionTable",
+        ///     "MyVersionTable");
         /// </example>
         public SqlAnywhereTableJournal(Func<IConnectionManager> connectionManager, Func<IUpgradeLog> logger, string schema, string table, IScriptPreprocessor sqlProcessor)
             : base(connectionManager, logger, schema, table)
@@ -32,10 +30,10 @@ namespace DbUp.Support.SqlAnywhere
 
         protected override bool VerifyTableExistsCommand(IDbCommand command, string tableName, string schemaName)
         {
-            command.CommandText = string.Format("select object_id('{0}')", CreateTableName(schemaName, tableName));
+            command.CommandText = string.Format("SELECT COUNT(SYS.SYSTABLE.table_name) FROM SYS.SYSTABLE WHERE SYS.SYSTABLE.table_name = '{0}'", CreateTableName(null, tableName));
             command.CommandType = CommandType.Text;
             var result = command.ExecuteScalar() as int?;
-            return result.HasValue;
+            return result.GetValueOrDefault() > 0;
         }
     }
 }
