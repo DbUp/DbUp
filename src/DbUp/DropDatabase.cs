1564c2d11f08aee524e7249c4a4e974dd524b998
Dennis Roche <dennisroche@outlook.com>
2016-09-21 09:00:02 +0800

    Add support for drop database

---
+++ src/DbUp/DropDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DbUp
+{
+    /// <summary>
+    /// A fluent interface for dropping the target database.
+    /// </summary>
+    public static class DropDatabase
+    {
+        private static readonly SupportedDatabasesForDropDatabase Instance = new SupportedDatabasesForDropDatabase();
+
+        /// <summary>
+        /// Returns the databases supported by DbUp's DropDatabase feature.
+        /// </summary>
+        public static SupportedDatabasesForDropDatabase For
+        {
+            get { return Instance; }
+        }
+    }
+}
---
+++ src/DbUp/SupportedDatabasesForDropDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DbUp
+{
+    /// <summary>
+    /// Add extension methods to this type if you plan to add support for DropDatabase for additional databases.
+    /// </summary>
+    public class SupportedDatabasesForDropDatabase
+    {
+    }
+}
