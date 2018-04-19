eab83aa9f89bd452fde6083783f3097c8ec7e483
Joe Phillips <joe.phillips@coinigy.com>
2018-04-19 10:35:24 -0500

    #169 - Fix the IsCustomStatement check. The prior Read() was setting CurrentChar but then this method was Peeking to the next char and treating it as the beginning. Also made a test to check for this scenario.

--- src/dbup-tests/Support/MySql/MySqlConnectionManagerTests.cs
+++ src/dbup-tests/Support/MySql/MySqlConnectionManagerTests.cs
@@ -55,5 +55,23 @@ namespace DbUp.Tests.Support.MySql
             enumerable[2].IndexOf("DELIMITER", StringComparison.Ordinal).ShouldBe(-1);
             enumerable[3].IndexOf("DELIMITER", StringComparison.Ordinal).ShouldBe(-1);
         }
-    }
+
+		[Fact]
+		public void ParsesOutBeginningDelimiter()
+		{
+			var multiCommand = "DELIMITER $$";
+			multiCommand += Environment.NewLine;
+			multiCommand += "CREATE TABLE 'ZIP'$$";
+			multiCommand += Environment.NewLine;
+			multiCommand += "CREATE TABLE IF NOT EXISTS 'BAR';";
+
+			var connectionManager = new MySqlConnectionManager("connectionstring");
+			var result = connectionManager.SplitScriptIntoCommands(multiCommand);
+
+			var enumerable = result as string[] ?? result.ToArray();
+			enumerable.Length.ShouldBe(2);
+			enumerable[0].IndexOf("DELIMITER", StringComparison.Ordinal).ShouldBe(-1);
+			enumerable[1].IndexOf("DELIMITER", StringComparison.Ordinal).ShouldBe(-1);
+		}
+	}
 }
