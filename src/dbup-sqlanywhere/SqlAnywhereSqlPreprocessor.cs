f11dcd5f1a2db0b44d9c9dfd152f160e49cb5d87
Alexandre Furlaneto <afurlanet@yahoo.com>
2018-07-06 14:58:09 +0100

    Added SQL Anywhere support.

---
+++ src/dbup-sqlanywhere/SqlAnywhereSqlPreprocessor.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using DbUp.Engine;
+
+namespace DbUp.SqlAnywhere
+{
+    /// <summary>
+    /// This preprocessor makes adjustments to your sql to make it compatible with Sql Anywhere.
+    /// </summary>
+    public class SqlAnywhereSqlPreprocessor : IScriptPreprocessor
+    {
+        public string Process(string contents)
+        {
+            return contents;
+        }
+    }
+}
