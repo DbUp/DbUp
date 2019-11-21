5ef3cd2e9b8a95944f5228de0f8b405615326af0
Justin J Stark <jstark@arborday.org>
2019-11-21 15:24:30 -0600

    Allow use of Azure AppAuthentication integrated security

--- src/dbup-sqlserver/SqlConnectionManager.cs
+++ src/dbup-sqlserver/SqlConnectionManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using DbUp.Engine.Transactions;
 using DbUp.Support;
+#if SUPPORTS_AZURE_AD
+using Microsoft.Azure.Services.AppAuthentication;
+#endif
 
 namespace DbUp.SqlServer
 {
@@ -15,10 +18,18 @@ namespace DbUp.SqlServer
         /// Manages Sql Database Connections
         /// </summary>
         /// <param name="connectionString"></param>
-        public SqlConnectionManager(string connectionString)
+        public SqlConnectionManager(string connectionString, bool useAzureSqlIntegratedSecurity)
             : base(new DelegateConnectionFactory((log, dbManager) =>
             {
                 var conn = new SqlConnection(connectionString);
+
+#if SUPPORTS_AZURE_AD
+                if(useAzureSqlIntegratedSecurity)
+                    conn.AccessToken = (new AzureServiceTokenProvider()).GetAccessTokenAsync("https://database.windows.net/").GetAwaiter().GetResult();
+#else
+                if(useAzureSqlIntegratedSecurity)
+                    throw new Exception("You are targeting a framework that does not support Azure AppAuthentication. The minimum target frameworks are .NET Framework 4.5.2 and .NET Standard 1.4.");
+#endif
 
                 if (dbManager.IsScriptOutputLogged)
                     conn.InfoMessage += (sender, e) => log.WriteInformation($"{{0}}{Environment.NewLine}", e.Message);
