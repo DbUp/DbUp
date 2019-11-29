8bb4af2062c64afb7b2ab17ed2ae7517e2a7b134
John Simons <john.simons@octopus.com>
2019-11-29 11:33:11 +1000

    Update to Microsoft.Data.SqlClient

--- src/dbup-sqlserver/Helpers/TemporarySqlDatabase.cs
+++ src/dbup-sqlserver/Helpers/TemporarySqlDatabase.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using DbUp.Helpers;
 
 namespace DbUp.SqlServer.Helpers
--- src/dbup-sqlserver/SqlConnectionManager.cs
+++ src/dbup-sqlserver/SqlConnectionManager.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using DbUp.Engine.Transactions;
 using DbUp.Support;
 #if SUPPORTS_AZURE_AD
