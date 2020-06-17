f555cc432b7a8d035bf50f989d991b147b973e97
Robert Wagner <robert@wagner.id.au>
2020-06-17 14:38:24 +1000

    Changed the Oracle implementation to split on `/` instead of `;`.
    Based on  #335

--- src/dbup-oracle/OracleCommandSplitter.cs
+++ src/dbup-oracle/OracleCommandSplitter.cs
@@ -1,9 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using DbUp.Support;
 
 namespace DbUp.Oracle
 {
     public class OracleCommandSplitter
     {
+        private readonly Func<string, SqlCommandReader> commandReaderFactory;
+
+        [Obsolete]
+        public OracleCommandSplitter()
+        {
+            this.commandReaderFactory = scriptContents => new OracleCommandReader(scriptContents);
+        }
+        
+        public OracleCommandSplitter(char delimiter)
+        {
+            this.commandReaderFactory = scriptContents => new OracleCustomDelimiterCommandReader(scriptContents, delimiter);
+        }
+        
         /// <summary>
         /// Splits a script with multiple delimited commands into commands
         /// </summary>
@@ -11,7 +26,7 @@ namespace DbUp.Oracle
         /// <returns></returns>
         public IEnumerable<string> SplitScriptIntoCommands(string scriptContents)
         {
-            using (var reader = new OracleCommandReader(scriptContents))
+            using (var reader = commandReaderFactory(scriptContents))
             {
                 var commands = new List<string>();
                 reader.ReadAllCommands(c => commands.Add(c));
