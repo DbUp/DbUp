ddd1b21e9a1d9d50f0fe4e8e6236873d0c94f2d7
raph <me@raph.ws>
2022-10-26 16:58:46 +1100

    Add Azure via net6.0

--- src/dbup-sqlserver/SqlServerExtensions.cs
+++ src/dbup-sqlserver/SqlServerExtensions.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Data;
+#if SUPPORTS_MICROSOFT_SQL_CLIENT
+using Microsoft.Data.SqlClient;
+#else
 using System.Data.SqlClient;
+#endif
 using DbUp;
 using DbUp.Builder;
 using DbUp.Engine.Output;
