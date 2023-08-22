c40c56f772633cffe570e20b5a3343e6676fc5cd
Chris Kelly <chris.kelly@wexinc.com>
2023-08-22 11:54:59 +1000

    Convert to file-scoped namespace

--- src/dbup-tests/Support/Postgresql/PostgresTableJournalTests.cs
+++ src/dbup-tests/Support/Postgresql/PostgresTableJournalTests.cs
@@ -8,89 +8,88 @@ using NSubstitute;
 using Shouldly;
 using Xunit;
 
-namespace DbUp.Tests.Support.Postgresql
+namespace DbUp.Tests.Support.Postgresql;
+
+public class PostgresTableJournalTests
 {
-    public class PostgresTableJournalTests
+    [Fact]
+    public void uses_named_parameters_when_sql_rewriting_not_specified()
     {
-        [Fact]
-        public void uses_named_parameters_when_sql_rewriting_not_specified()
-        {
-            // Arrange
-            var dbConnection = Substitute.For<IDbConnection>();
-            var connectionManager = new TestConnectionManager(dbConnection, true);
-            var command = Substitute.For<IDbCommand>();
-            var param1 = Substitute.For<IDbDataParameter>();
-            var param2 = Substitute.For<IDbDataParameter>();
-            dbConnection.CreateCommand().Returns(command);
-            command.CreateParameter().Returns(param1, param2);
-            command.ExecuteScalar().Returns(x => 0);
-            var consoleUpgradeLog = new ConsoleUpgradeLog();
-            var journal = new PostgresqlTableJournal(() => connectionManager, () => consoleUpgradeLog, "public", "SchemaVersions");
+        // Arrange
+        var dbConnection = Substitute.For<IDbConnection>();
+        var connectionManager = new TestConnectionManager(dbConnection, true);
+        var command = Substitute.For<IDbCommand>();
+        var param1 = Substitute.For<IDbDataParameter>();
+        var param2 = Substitute.For<IDbDataParameter>();
+        dbConnection.CreateCommand().Returns(command);
+        command.CreateParameter().Returns(param1, param2);
+        command.ExecuteScalar().Returns(x => 0);
+        var consoleUpgradeLog = new ConsoleUpgradeLog();
+        var journal = new PostgresqlTableJournal(() => connectionManager, () => consoleUpgradeLog, "public", "SchemaVersions");
 
-            // Act
-            journal.StoreExecutedScript(new SqlScript("test", "select 1"), () => command);
+        // Act
+        journal.StoreExecutedScript(new SqlScript("test", "select 1"), () => command);
 
-            // Assert
-            command.Received(2).CreateParameter();
-            param1.ParameterName.ShouldBe("scriptName");
-            param2.ParameterName.ShouldBe("applied");
-            command.CommandText.ShouldBe("""insert into "public"."SchemaVersions" (ScriptName, Applied) values (@scriptName, @applied)""");
-            command.Received().ExecuteNonQuery();
-        }
+        // Assert
+        command.Received(2).CreateParameter();
+        param1.ParameterName.ShouldBe("scriptName");
+        param2.ParameterName.ShouldBe("applied");
+        command.CommandText.ShouldBe("""insert into "public"."SchemaVersions" (ScriptName, Applied) values (@scriptName, @applied)""");
+        command.Received().ExecuteNonQuery();
+    }
 
-        [Fact]
-        public void uses_positional_parameters_when_sql_rewriting_disabled()
-        {
-            AppContext.SetSwitch("Npgsql.EnableSqlRewriting", false);
+    [Fact]
+    public void uses_positional_parameters_when_sql_rewriting_disabled()
+    {
+        AppContext.SetSwitch("Npgsql.EnableSqlRewriting", false);
 
-            var dbConnection = Substitute.For<IDbConnection>();
-            var connectionManager = new TestConnectionManager(dbConnection, true);
-            var command = Substitute.For<IDbCommand>();
-            var param1 = Substitute.For<IDbDataParameter>();
-            var param2 = Substitute.For<IDbDataParameter>();
-            dbConnection.CreateCommand().Returns(command);
-            command.CreateParameter().Returns(param1, param2);
-            command.ExecuteScalar().Returns(x => 0);
-            var consoleUpgradeLog = new ConsoleUpgradeLog();
-            var journal = new PostgresqlTableJournal(() => connectionManager, () => consoleUpgradeLog, "public", "SchemaVersions");
+        var dbConnection = Substitute.For<IDbConnection>();
+        var connectionManager = new TestConnectionManager(dbConnection, true);
+        var command = Substitute.For<IDbCommand>();
+        var param1 = Substitute.For<IDbDataParameter>();
+        var param2 = Substitute.For<IDbDataParameter>();
+        dbConnection.CreateCommand().Returns(command);
+        command.CreateParameter().Returns(param1, param2);
+        command.ExecuteScalar().Returns(x => 0);
+        var consoleUpgradeLog = new ConsoleUpgradeLog();
+        var journal = new PostgresqlTableJournal(() => connectionManager, () => consoleUpgradeLog, "public", "SchemaVersions");
 
-            // Act
-            journal.StoreExecutedScript(new SqlScript("test", "select 1"), () => command);
+        // Act
+        journal.StoreExecutedScript(new SqlScript("test", "select 1"), () => command);
 
-            // Assert
-            command.Received(2).CreateParameter();
-            param1.ParameterName.ShouldBeNullOrEmpty();
-            param2.ParameterName.ShouldBeNullOrEmpty();
-            command.CommandText.ShouldBe("""insert into "public"."SchemaVersions" (ScriptName, Applied) values ($1, $2)""");
-            command.Received().ExecuteNonQuery();
-        }
+        // Assert
+        command.Received(2).CreateParameter();
+        param1.ParameterName.ShouldBeNullOrEmpty();
+        param2.ParameterName.ShouldBeNullOrEmpty();
+        command.CommandText.ShouldBe("""insert into "public"."SchemaVersions" (ScriptName, Applied) values ($1, $2)""");
+        command.Received().ExecuteNonQuery();
+    }
 
-        [Fact]
-        public void uses_named_parameters_when_sql_rewriting_enabled()
-        {
-            // Arrange
-            AppContext.SetSwitch("Npgsql.EnableSqlRewriting", true);
+    [Fact]
+    public void uses_named_parameters_when_sql_rewriting_enabled()
+    {
+        // Arrange
+        AppContext.SetSwitch("Npgsql.EnableSqlRewriting", true);
 
-            var dbConnection = Substitute.For<IDbConnection>();
-            var connectionManager = new TestConnectionManager(dbConnection, true);
-            var command = Substitute.For<IDbCommand>();
-            var param1 = Substitute.For<IDbDataParameter>();
-            var param2 = Substitute.For<IDbDataParameter>();
-            dbConnection.CreateCommand().Returns(command);
-            command.CreateParameter().Returns(param1, param2);
-            command.ExecuteScalar().Returns(x => 0);
-            var consoleUpgradeLog = new ConsoleUpgradeLog();
-            var journal = new PostgresqlTableJournal(() => connectionManager, () => consoleUpgradeLog, "public", "SchemaVersions");
+        var dbConnection = Substitute.For<IDbConnection>();
+        var connectionManager = new TestConnectionManager(dbConnection, true);
+        var command = Substitute.For<IDbCommand>();
+        var param1 = Substitute.For<IDbDataParameter>();
+        var param2 = Substitute.For<IDbDataParameter>();
+        dbConnection.CreateCommand().Returns(command);
+        command.CreateParameter().Returns(param1, param2);
+        command.ExecuteScalar().Returns(x => 0);
+        var consoleUpgradeLog = new ConsoleUpgradeLog();
+        var journal = new PostgresqlTableJournal(() => connectionManager, () => consoleUpgradeLog, "public", "SchemaVersions");
 
-            // Act
-            journal.StoreExecutedScript(new SqlScript("test", "select 1"), () => command);
+        // Act
+        journal.StoreExecutedScript(new SqlScript("test", "select 1"), () => command);
 
-            // Assert
-            command.Received(2).CreateParameter();
-            param1.ParameterName.ShouldBe("scriptName");
-            param2.ParameterName.ShouldBe("applied");
-            command.CommandText.ShouldBe("""insert into "public"."SchemaVersions" (ScriptName, Applied) values (@scriptName, @applied)""");
-            command.Received().ExecuteNonQuery();
-        }
+        // Assert
+        command.Received(2).CreateParameter();
+        param1.ParameterName.ShouldBe("scriptName");
+        param2.ParameterName.ShouldBe("applied");
+        command.CommandText.ShouldBe("""insert into "public"."SchemaVersions" (ScriptName, Applied) values (@scriptName, @applied)""");
+        command.Received().ExecuteNonQuery();
     }
 }
