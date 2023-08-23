ab1d330d1a191631698cf3aefb00e39dcd146bb5
Jason Finch <jason.finch@gmail.com>
2023-08-23 18:35:27 +1000

    fix (tests): Ensure PostgreSQL tests reset EnableSqlRewriting (default) to true after run.

--- src/Tests.Common/RecordingDb/RecordingDataParameterCollection.cs
+++ src/Tests.Common/RecordingDb/RecordingDataParameterCollection.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
@@ -32,7 +32,7 @@ namespace DbUp.Tests.Common.RecordingDb
         public bool IsSynchronized { get; private set; }
         public int Add(object value)
         {
-            logger.WriteInformation(string.Format("DB Operation: Add parameter to command: {0}", value));
+            logger.WriteInformation($"DB Operation: Add parameter to command: {value}");
             backingList.Add(value);
             return backingList.Count - 1;
         }
