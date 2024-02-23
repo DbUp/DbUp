3606ef6667ae58010d663795648449a7e16da4e9
Robert Wagner <robert.wagner@octopus.com>
2024-02-23 17:23:18 +1000

    Fix merge problem

--- src/Tests.Common/RecordingDb/RecordingDataParameterCollection.cs
+++ src/Tests.Common/RecordingDb/RecordingDataParameterCollection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Data;
 using DbUp.Engine.Output;
 
@@ -9,12 +10,12 @@ namespace DbUp.Tests.Common.RecordingDb;
 class RecordingDataParameterCollection : IDataParameterCollection
 {
     readonly IUpgradeLog logger;
-    readonly List<object> backingList;
+    readonly ConcurrentBag<object?> backingList;
 
     public RecordingDataParameterCollection(IUpgradeLog logger)
     {
         this.logger = logger;
-        backingList = new List<object>();
+        backingList = new ConcurrentBag<object?>();
     }
 
     public IEnumerator GetEnumerator()
