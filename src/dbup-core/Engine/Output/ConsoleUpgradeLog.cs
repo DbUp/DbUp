afa198725ada6fe6de9c263b4e3262841782626a
Robert Wagner <robert.wagner@octopus.com>
2024-02-23 17:10:04 +1000

    Renamed constants class

--- src/dbup-core/Engine/Output/ConsoleUpgradeLog.cs
+++ src/dbup-core/Engine/Output/ConsoleUpgradeLog.cs
@@ -9,27 +9,27 @@ public class ConsoleUpgradeLog : IUpgradeLog
 {
     /// <summary>
     public void LogTrace(string format, params object[] args)
-        => Log(Constants.TraceLevel, ConsoleColor.Gray, format, args);
+        => Log(LoggingConstants.TraceLevel, ConsoleColor.Gray, format, args);
 
     /// <inheritdoc/>
     public void LogDebug(string format, params object[] args)
-        => Log(Constants.DebugLevel, ConsoleColor.Magenta, format, args);
+        => Log(LoggingConstants.DebugLevel, ConsoleColor.Magenta, format, args);
 
     /// <inheritdoc/>
     public void LogInformation(string format, params object[] args)
-        => Log(Constants.InfoLevel, ConsoleColor.White, format, args);
+        => Log(LoggingConstants.InfoLevel, ConsoleColor.White, format, args);
 
     /// <inheritdoc/>
     public void LogWarning(string format, params object[] args)
-        => Log(Constants.WarnLevel, ConsoleColor.Yellow, format, args);
+        => Log(LoggingConstants.WarnLevel, ConsoleColor.Yellow, format, args);
 
     /// <inheritdoc/>
     public void LogError(string format, params object[] args)
-        => Log(Constants.ErrorLevel, ConsoleColor.Red, format, args);
+        => Log(LoggingConstants.ErrorLevel, ConsoleColor.Red, format, args);
 
     /// <inheritdoc/>
     public void LogError(Exception ex, string format, params object[] args)
-        => Log(Constants.ErrorLevel, ConsoleColor.Red, format, args, ex);
+        => Log(LoggingConstants.ErrorLevel, ConsoleColor.Red, format, args, ex);
 
     static void Log(string level, ConsoleColor color, string format, object[] args, Exception ex = null)
     {
@@ -37,7 +37,7 @@ public class ConsoleUpgradeLog : IUpgradeLog
         Console.ForegroundColor = color;
         try
         {
-            Console.WriteLine($"{DateTimeOffset.Now.ToString(Constants.TimestampFormat)} [{level}] {string.Format(format, args)}");
+            Console.WriteLine($"{DateTimeOffset.Now.ToString(LoggingConstants.TimestampFormat)} [{level}] {string.Format(format, args)}");
             if (ex != null)
             {
                 Console.WriteLine(ExceptionFormatter.Format(ex));
--- src/dbup-core/Engine/Output/TraceUpgradeLog.cs
+++ src/dbup-core/Engine/Output/TraceUpgradeLog.cs
@@ -10,31 +10,31 @@ public class TraceUpgradeLog : IUpgradeLog
 {
     /// <summary>
     public void LogTrace(string format, params object[] args)
-        => Log(Constants.TraceLevel, format, args);
+        => Log(LoggingConstants.TraceLevel, format, args);
 
     /// <inheritdoc/>
     public void LogDebug(string format, params object[] args)
-        => Log(Constants.DebugLevel, format, args);
+        => Log(LoggingConstants.DebugLevel, format, args);
 
     /// <inheritdoc/>
     public void LogInformation(string format, params object[] args)
-        => Log(Constants.InfoLevel, format, args);
+        => Log(LoggingConstants.InfoLevel, format, args);
 
     /// <inheritdoc/>
     public void LogWarning(string format, params object[] args)
-        => Log(Constants.WarnLevel, format, args);
+        => Log(LoggingConstants.WarnLevel, format, args);
 
     /// <inheritdoc/>
     public void LogError(string format, params object[] args)
-        => Log(Constants.ErrorLevel, format, args);
+        => Log(LoggingConstants.ErrorLevel, format, args);
 
     /// <inheritdoc/>
     public void LogError(Exception ex, string format, params object[] args)
-        => Log(Constants.ErrorLevel, format, args, ex);
+        => Log(LoggingConstants.ErrorLevel, format, args, ex);
 
     static void Log(string level, string format, object[] args, Exception ex = null)
     {
-        Trace.WriteLine($"{DateTimeOffset.Now.ToString(Constants.TimestampFormat)} [{level}] {string.Format(format, args)}");
+        Trace.WriteLine($"{DateTimeOffset.Now.ToString(LoggingConstants.TimestampFormat)} [{level}] {string.Format(format, args)}");
         if (ex != null)
             /// <summary>
             /// Writes an error message to the log.
